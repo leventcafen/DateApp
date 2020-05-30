74176ba5c84973263fce42e12bb3633715d010af
leventcafen <leventcafen@hotmail.com>
2020-05-31 00:04:55 +0300

    User Service and Userlist Photo Css

--- DataApp-SPA/DataApp-SPA/src/app/app.module.ts
+++ DataApp-SPA/DataApp-SPA/src/app/app.module.ts
@@ -14,11 +14,18 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+
 import { appRoutes } from './routes';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+
+export function tokenGetter(){
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -28,7 +35,8 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent 
    ],
    imports: [
       BrowserModule,
@@ -36,7 +44,14 @@ import { appRoutes } from './routes';
       FormsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config:{
+            tokenGetter:tokenGetter,
+            whitelistedDomains:['localhost:5000'],
+            blacklistedRoutes:['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
--- DataApp-SPA/DataApp-SPA/src/app/routes.ts
+++ DataApp-SPA/DataApp-SPA/src/app/routes.ts
@@ -1,9 +1,9 @@
 import{Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberListComponent } from './members/member-list/member-list.component';
 
 export const appRoutes:Routes=[
     {path:'home',component:HomeComponent},
